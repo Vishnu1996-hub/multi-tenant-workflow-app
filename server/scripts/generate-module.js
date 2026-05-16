c7bea0a92795a505386313020b7a39c80131cf97
Vishnu1996-hub <vishnu28496@gmail.com>
2026-05-16 14:14:49 +0530

    module generate script

---
+++ server/scripts/generate-module.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const path = require("path");
+
+const folderName = process.argv[2];
+
+if (!folderName) {
+  console.error("Please provide module name");
+  process.exit(1);
+}
+
+const filePrefix = folderName.endsWith("s")
+  ? folderName.slice(0, -1)
+  : folderName;
+
+const basePath = path.join(__dirname, "..", "src", "modules", folderName);
+
+const files = [
+  `${filePrefix}.router.ts`,
+  `${filePrefix}.controller.ts`,
+  `${filePrefix}.service.ts`,
+  `${filePrefix}.repository.ts`,
+  `${filePrefix}.schema.ts`,
+  `${filePrefix}.types.ts`,
+];
+
+fs.mkdirSync(basePath, { recursive: true });
+
+files.forEach((file) => {
+  fs.writeFileSync(path.join(basePath, file), "", { flag: "w" });
+});
+
+console.log(`Generated module: ${folderName}`);
